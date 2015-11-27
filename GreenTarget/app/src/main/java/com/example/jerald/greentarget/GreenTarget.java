8e398d95307e34ea3d43e34d91505cd3cec0c536
Jerald Limqueco <jeraldlimqueco@yahoo.com>
2015-11-28 02:18:09 +0800

    green target

---
+++ GreenTarget/app/src/main/java/com/example/jerald/greentarget/GreenTarget.java
@@ -0,0 +1,36 @@
+package com.example.jerald.greentarget;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.util.AttributeSet;
+import android.view.View;
+
+/**
+ * Created by Jerald on 11/28/2015.
+ */
+public class GreenTarget  extends View {
+
+    public GreenTarget(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+
+        Paint green = new Paint();
+        green.setARGB(255, 0, 255, 0);
+
+        Paint white = new Paint();
+        white.setARGB(255, 255, 255, 255);
+
+        int w = canvas.getWidth();
+        int h = canvas.getHeight();
+        int x = (w < h)? w : h;
+
+        for (int i = 0; i < 11; i++) {
+            canvas.drawCircle(w/2, h/2, x*(10-i)/20, i % 2 == 0 ? green : white);
+        }
+    }
+}
